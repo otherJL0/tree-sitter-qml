90decea1be5004b73ea1635766262c72c56773f3
otherJL0 <jonathanglopez@gmail.com>
2021-10-21 18:27:02 +0000

    Adding basic import grammar

--- grammar.js
+++ grammar.js
@@ -1,8 +1,30 @@
 module.exports = grammar({
-  name: 'qml',
+  name: "qml",
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => 'hello'
-  }
+    identifier: ($) => /[A-Za-z]+/,
+
+    number: ($) => /\d+/,
+
+    import_statement: ($) =>
+      seq(
+        "import",
+        $._module_identifier,
+        $._version_number,
+        optional(seq("as", $.identifier)),
+      ),
+
+    _module_identifier: ($) =>
+      seq(
+        $.identifier,
+        optional("."),
+      ),
+
+    _version_number: ($) =>
+      seq(
+        $.number,
+        ".",
+        $.number,
+      ),
+  },
 });
--- src/parser.c
+++ src/parser.c
@@ -8,29 +8,41 @@
 #define LANGUAGE_VERSION 13
 #define STATE_COUNT 4
 #define LARGE_STATE_COUNT 2
-#define SYMBOL_COUNT 3
+#define SYMBOL_COUNT 7
 #define ALIAS_COUNT 0
-#define TOKEN_COUNT 2
+#define TOKEN_COUNT 6
 #define EXTERNAL_TOKEN_COUNT 0
 #define FIELD_COUNT 0
 #define MAX_ALIAS_SEQUENCE_LENGTH 1
 #define PRODUCTION_ID_COUNT 1
 
 enum {
-  anon_sym_hello = 1,
-  sym_source_file = 2,
+  aux_sym_identifier_token1 = 1,
+  sym_number = 2,
+  anon_sym_import = 3,
+  anon_sym_as = 4,
+  anon_sym_DOT = 5,
+  sym_identifier = 6,
 };
 
 static const char * const ts_symbol_names[] = {
   [ts_builtin_sym_end] = "end",
-  [anon_sym_hello] = "hello",
-  [sym_source_file] = "source_file",
+  [aux_sym_identifier_token1] = "identifier_token1",
+  [sym_number] = "number",
+  [anon_sym_import] = "import",
+  [anon_sym_as] = "as",
+  [anon_sym_DOT] = ".",
+  [sym_identifier] = "identifier",
 };
 
 static const TSSymbol ts_symbol_map[] = {
   [ts_builtin_sym_end] = ts_builtin_sym_end,
-  [anon_sym_hello] = anon_sym_hello,
-  [sym_source_file] = sym_source_file,
+  [aux_sym_identifier_token1] = aux_sym_identifier_token1,
+  [sym_number] = sym_number,
+  [anon_sym_import] = anon_sym_import,
+  [anon_sym_as] = anon_sym_as,
+  [anon_sym_DOT] = anon_sym_DOT,
+  [sym_identifier] = sym_identifier,
 };
 
 static const TSSymbolMetadata ts_symbol_metadata[] = {
@@ -38,11 +50,27 @@ static const TSSymbolMetadata ts_symbol_metadata[] = {
     .visible = false,
     .named = true,
   },
-  [anon_sym_hello] = {
+  [aux_sym_identifier_token1] = {
+    .visible = false,
+    .named = false,
+  },
+  [sym_number] = {
+    .visible = true,
+    .named = true,
+  },
+  [anon_sym_import] = {
     .visible = true,
     .named = false,
   },
-  [sym_source_file] = {
+  [anon_sym_as] = {
+    .visible = true,
+    .named = false,
+  },
+  [anon_sym_DOT] = {
+    .visible = true,
+    .named = false,
+  },
+  [sym_identifier] = {
     .visible = true,
     .named = true,
   },
@@ -61,30 +89,62 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   eof = lexer->eof(lexer);
   switch (state) {
     case 0:
-      if (eof) ADVANCE(5);
-      if (lookahead == 'h') ADVANCE(1);
+      if (eof) ADVANCE(8);
+      if (lookahead == '.') ADVANCE(13);
+      if (lookahead == 'a') ADVANCE(5);
+      if (lookahead == 'i') ADVANCE(1);
       if (lookahead == '\t' ||
           lookahead == '\n' ||
           lookahead == '\r' ||
           lookahead == ' ') SKIP(0)
+      if (('0' <= lookahead && lookahead <= '9')) ADVANCE(10);
       END_STATE();
     case 1:
-      if (lookahead == 'e') ADVANCE(3);
+      if (lookahead == 'm') ADVANCE(3);
       END_STATE();
     case 2:
-      if (lookahead == 'l') ADVANCE(4);
+      if (lookahead == 'o') ADVANCE(4);
       END_STATE();
     case 3:
-      if (lookahead == 'l') ADVANCE(2);
+      if (lookahead == 'p') ADVANCE(2);
       END_STATE();
     case 4:
-      if (lookahead == 'o') ADVANCE(6);
+      if (lookahead == 'r') ADVANCE(6);
       END_STATE();
     case 5:
-      ACCEPT_TOKEN(ts_builtin_sym_end);
+      if (lookahead == 's') ADVANCE(12);
       END_STATE();
     case 6:
-      ACCEPT_TOKEN(anon_sym_hello);
+      if (lookahead == 't') ADVANCE(11);
+      END_STATE();
+    case 7:
+      if (lookahead == '\t' ||
+          lookahead == '\n' ||
+          lookahead == '\r' ||
+          lookahead == ' ') SKIP(7)
+      if (('A' <= lookahead && lookahead <= 'Z') ||
+          ('a' <= lookahead && lookahead <= 'z')) ADVANCE(9);
+      END_STATE();
+    case 8:
+      ACCEPT_TOKEN(ts_builtin_sym_end);
+      END_STATE();
+    case 9:
+      ACCEPT_TOKEN(aux_sym_identifier_token1);
+      if (('A' <= lookahead && lookahead <= 'Z') ||
+          ('a' <= lookahead && lookahead <= 'z')) ADVANCE(9);
+      END_STATE();
+    case 10:
+      ACCEPT_TOKEN(sym_number);
+      if (('0' <= lookahead && lookahead <= '9')) ADVANCE(10);
+      END_STATE();
+    case 11:
+      ACCEPT_TOKEN(anon_sym_import);
+      END_STATE();
+    case 12:
+      ACCEPT_TOKEN(anon_sym_as);
+      END_STATE();
+    case 13:
+      ACCEPT_TOKEN(anon_sym_DOT);
       END_STATE();
     default:
       return false;
@@ -93,7 +153,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
 
 static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
-  [1] = {.lex_state = 0},
+  [1] = {.lex_state = 7},
   [2] = {.lex_state = 0},
   [3] = {.lex_state = 0},
 };
@@ -101,11 +161,14 @@ static const TSLexMode ts_lex_modes[STATE_COUNT] = {
 static const uint16_t ts_parse_table[LARGE_STATE_COUNT][SYMBOL_COUNT] = {
   [0] = {
     [ts_builtin_sym_end] = ACTIONS(1),
-    [anon_sym_hello] = ACTIONS(1),
+    [sym_number] = ACTIONS(1),
+    [anon_sym_import] = ACTIONS(1),
+    [anon_sym_as] = ACTIONS(1),
+    [anon_sym_DOT] = ACTIONS(1),
   },
   [1] = {
-    [sym_source_file] = STATE(3),
-    [anon_sym_hello] = ACTIONS(3),
+    [sym_identifier] = STATE(3),
+    [aux_sym_identifier_token1] = ACTIONS(3),
   },
 };
 
@@ -127,7 +190,7 @@ static const TSParseActionEntry ts_parse_actions[] = {
   [0] = {.entry = {.count = 0, .reusable = false}},
   [1] = {.entry = {.count = 1, .reusable = false}}, RECOVER(),
   [3] = {.entry = {.count = 1, .reusable = true}}, SHIFT(2),
-  [5] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_source_file, 1),
+  [5] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_identifier, 1),
   [7] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
 };
 
